3484dbc80d9605abddebd04946cbdddf4c1e780d
BananaJeans <18456310+BananaJeanss@users.noreply.github.com>
2026-09-18 17:05:06 +0300

    Release Pocket Pitwall 0.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "dev.bananajeans.pitwall"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.2.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
